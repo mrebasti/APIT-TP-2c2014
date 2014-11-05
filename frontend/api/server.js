397329b9ec7160a725997b922a55c3b9a37a82d6
bordanzig <bordanzi.gonzalo92@gmail.com>
2014-11-05 14:29:11 -0300

    changed api to frontend

--- frontend/api/server.js
+++ frontend/api/server.js
@@ -105,4 +105,3 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
